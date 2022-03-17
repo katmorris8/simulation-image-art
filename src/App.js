7881bfdc41035e12d9916cf47df75e9689fbceb1
Kat Morris <kathryn.leigh.morris@gmail.com>
2022-03-17 15:57:04 -0400

    made an array for the rows based on how many squares can fit in the image height

--- src/App.js
+++ src/App.js
@@ -1,37 +1,32 @@
-import './App.css';
-import image from './images/test-pic.jpg'
-import Row from './Row';
+import "./App.css";
+import image from "./images/test-pic.jpg";
+import Row from "./Row";
 
 function App() {
-  const newImage = new Image()
+  const newImage = new Image();
   newImage.src = image;
+  const { src } = newImage;
   const scale = 4;
   const width = newImage.width / scale;
   const height = newImage.height / scale;
 
-  console.log('image dimensions', width, height);
-  const { src } = newImage;
-
-  // const styles = {
-  //   backgroundImage: `url(${src})`,
-  //   backgroundSize: `${width}px ${height}px`,
-  //   width: `${width / scale}px`,
-  //   height: `${width / scale}px`,
-  //   backgroundPosition: `-${(width / scale) * count}px 0`,
-  // }
-  // const imageStyle2 = {
-  //   backgroundPosition: `-${width / scale}px 0`,
-  // }
-  // const imageStyle3 = {
-  //   backgroundPosition: `-${(width / scale) * 2}px 0`,
-  // }
-  // const imageStyle4 = {
-  //   backgroundPosition: `-${(width / scale) * 3}px 0`,
-  // }
+  const numberOfTilesHigh = Math.floor(height / (width / scale));
+  const rowsArray = Array(numberOfTilesHigh).fill(<div />);
 
   return (
     <div className="App">
-      <Row src={src} scale={scale} width={width} height={height} />
+      {rowsArray.map((item, index) => {
+        return (
+          <Row
+            key={`row ${(src, index)}`}
+            src={src}
+            scale={scale}
+            width={width}
+            height={height}
+            rowIndex={index}
+          />
+        );
+      })}
     </div>
   );
 }
