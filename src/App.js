481a358e1de6019daedbf9c595dc98cd8026ddc0
Kat Morris <katmorris@Kats-MacBook-Pro.local>
2022-03-17 13:31:48 -0400

    insert and scale image

--- src/App.js
+++ src/App.js
@@ -3,30 +3,17 @@ import './App.css';
 import image from './images/test-pic.jpg'
 
 function App() {
-  const [img, setImg] = useState(new Image());
-  useEffect(() => {
-    // const img = new Image();
-    img.src = image;
-    return img;
-  })
-  console.log('image dimensions', img.width, img.height);
+  const newImage = new Image()
+  newImage.src = image;
+  const scale = 4;
+  const height = newImage.height / scale;
+  const width = newImage.width / scale;
+
+  console.log('image dimensions', width, height);
+
   return (
     <div className="App">
-      <img src={img} alt="poppy fields" />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <img src={newImage.src} height={height} width={width} alt="" />
     </div>
   );
 }
